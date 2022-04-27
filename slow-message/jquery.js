77aa3da62d97f6e7b65943d9ca4f24a5ecc40598
lucasclemerson <clemerson.lucas.oliveira@gmail.com>
2022-04-27 17:23:34 -0300

    init branch

--- slow-message/jquery.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	$('button').click(function(){
-		$('#mymessage').text($('input[name=input-text]').val());
-		$('#mymessage').fadeIn();
-		$('#mymessage').delay(2000);
-		$('#mymessage').fadeOut();
-	});	
-});

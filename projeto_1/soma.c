6fabdb9a682331a1eef33ca62fedb9bed4893b38
lucasclemerson <clemerson.lucas.oliveira@gmail.com>
2022-04-27 20:08:50 -0300

    código soma dois números

---
+++ projeto_1/soma.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main ()
+{
+  int n1, n2, soma;
+  printf("Informe o valor (n1): ");
+  scanf("%d", &n1);
+  printf("Informe o valor (n2): ");
+  scanf("%d", &n2);
+
+  soma = n1+n2;
+
+  printf("\nA soma de %d + %d = %d", n1,n2, soma);
+
+
+}

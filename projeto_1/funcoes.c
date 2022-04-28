f0183ed61008b323233366c366bc9e7b1dc53423
lucasclemerson <clemerson.lucas.oliveira@gmail.com>
2022-04-28 12:02:54 -0300

    funções aritimeticas

--- projeto_1/funcoes.c
+++ projeto_1/funcoes.c
@@ -1,73 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-#define true 1
-#define false 0
-#define pi 3.14
-
+#include <math.h>
 
 void main ()
 {
   float n1;
 
-  printf("Informe o valor (n1): ");
+  printf("Informe o um número: ");
   scanf("%f", &n1);
 
-
-  int inteiro, achou_raiz;
+  int inteiro;
   float expo, v_absoluto, raiz;
 
   expo = n1*n1;
   v_absoluto = abs(n1);
   inteiro = n1;
-
-  achou_raiz = false;
-  for (int i=0; i<=n1; i++)
-  {
-    if ((i * i) == n1)
-    {
-      achou_raiz = true;
-      printf("\nRAíZ DO NÚMERO: %d", i);
-    }
-  }
-
-  if (!achou_raiz)
-  {
-    printf("\nRAíZ DO NÚMERO: -1");
-  }
+  raiz = sqrt(n1);
 
 
-
+  printf("\nRAíZ DO NÚMERO: %f", raiz);
   printf("\nNÚMERO AO QUADRADO: %f", expo);
   printf("\nVALOR ABSOLUTO: %f", v_absoluto);
   printf("\nNÚMERO EM INTEIRO: %d", inteiro);
-  printf("\nPi: %f", pi);
-
-
-
-  /*
-  int n1, n2;
-  int soma, divisao, multiplicacao, subtracao;
-
-  printf("Informe o valor (n1): ");
-  scanf("%d", &n1);
-  printf("Informe o valor (n2): ");
-  scanf("%d", &n2);
-
-
-  soma = n1+n2;
-  subtracao = n1-n2;
-  divisao = n1/n2;
-  multiplicacao = n1*n2;
-
-
-  printf("\nSOMA; %d+%d = %d", n1,n2, soma);
-  printf("\nSUBTRAÇÃO; %d-%d = %d", n1,n2, subtracao);
-  printf("\nDIVISÃO; %d/%d = %d", n1,n2, divisao);
-  printf("\nMULTIPLICAÇÃO; %d*%d = %d", n1,n2, multiplicacao);
-
-  */
-
-
 
 }

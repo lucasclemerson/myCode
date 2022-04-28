73e372e1c65e43c26e078e297b5068b7912e3b01
lucasclemerson <clemerson.lucas.oliveira@gmail.com>
2022-04-28 12:03:06 -0300

    funções para angulos

---
+++ projeto_1/angulos.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main ()
+{
+  int angulo;
+  printf("Infome um valor de angulo:");
+  scanf("%d", &angulo);
+
+  float sen, cose, tang;
+
+  sen = sin(angulo);
+  cose = cos(angulo);
+  tang = tan(angulo);
+
+  printf("\nSEN: %.2f", sen);
+  printf("\nCOS: %.2f", cose);
+  printf("\nTAN: %.2f", tang);
+}

d03284c94b098900085ccb20edd7e39d8fefc2f8
lucasclemerson <clemerson.lucas.oliveira@gmail.com>
2022-04-28 12:31:59 -0300

    código para verificar o tipo de triângulos

---
+++ projeto_2/triangulos.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+
+void main ()
+{
+  int a, b, c;
+
+  printf("Informe os lados do seu triângulo");
+  printf("\nValor (a): ");
+  scanf("%d", &a);
+  printf("Valor (b): ");
+  scanf("%d", &b);
+  printf("Valor (c): ");
+  scanf("%d", &c);
+
+  int eh_equilatero = (a==b && b==c);
+  int eh_escaleno = (a!=b && b!=c && a!=c);
+  int eh_triangulo = (a < b+c) && (b < a+c) && (c < a+b);
+
+  printf("\né triângulo: %d", eh_triangulo);
+  printf("\né equilatero: %d", eh_equilatero);
+  printf("\né escaleno: %d", eh_escaleno);
+
+
+}
